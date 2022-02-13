1eb2b4e622d6c99460dffe16965b6c07f5c1721a
nisa <ulizulfaanisa@gmail.com>
2022-02-13 14:56:30 +0700

    update file main.py

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-a = int(input("masukkan angka"))
-b = int(input("masukkan angka"))
-hasil = a+b
-print(hasil)
+
+from class_calc import Calculator
+
+# program untuk menjalankan calculator
+while True:
+        
+    user_input = input("Tuliskan nilai yang ingin dihitung: ")
+    user_input = user_input.strip().split(" ")
+                
+    user_number1 = int(user_input[0])
+    user_number2 = int(user_input[2])
+    decision = user_input[1]
+                                
+    calc = Calculator(user_number1, user_number2)
+    result = calc.decision(decision)
+    print(result)
+                                                
+    lanjut = input("Anda ingin melanjutkan perhitungan?: ")
+                                                        
+    if lanjut.lower()[0] == 'y':
+        continue
+    else: break
